8e7ed4c9217c5306fb2f5c5bfd5de1d39723e4d3
Mohamed Helmy <mohammedahmedhelmy5@gmail.com>
2025-08-27 17:48:53 +0300

    js-1.js

--- js-1.js
+++ js-1.js
@@ -53,3 +53,39 @@ function noSpace(str) {
   return str.replace(/\s/g, '');
 }
 console.log(noSpace("Hello  World ")); 
+
+//19 Reversing Words in a String
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(reverseWords("I am here")); // "here am I"
+
+//20 Keep Hydrated
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+console.log(litres(3));  
+console.log(litres(6.7)); 
+console.log(litres(11.8)); 
+
+//21  Convert a Number to a String
+function numberToString(num) {
+  return num.toString();
+}
+
+console.log(numberToString(123));  
+console.log(numberToString(99));
+
+//22 Calculate Average
+function findAverage(arr) {
+  if (arr.length === 0) return 0; 
+  
+  let sum = arr.reduce((acc, num) => acc + num, 0);
+  return sum / arr.length;
+}
+
+console.log(findAverage([1, 2, 3, 4, 5])); 
+console.log(findAverage([10, 20, 30]));                
+
